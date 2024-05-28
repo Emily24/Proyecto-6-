cf25e7c08af518e035d5f26b8eb49b7cebbecca4
Emily Rocio Melgarejo Daza <emilydaza@gmail.com>
2024-05-27 21:52:33 -0500

    Mostrar pokemon en pantalla

--- script.js
+++ script.js
@@ -4,10 +4,43 @@
 //I: interface
 console.log('Peticiones')
 
+//callback
+const obtener = () => {
+    console.log('obtener')
+}
+
+const buttonObtener = document.getElementById('button-obtener')
+//Agregar evento al boton
+//buttonObtener.addEventListener("click", obtener)
+buttonObtener.addEventListener('click', async () => {
+    console.log('obtener')
+    const pokemonData = await obtenerPokemon()
+
+    const pokemonArticle = document.getElementById('pokemon')
+    //Crea un elemento HTML
+    const h3 = document.createElement('h3')
+    //le asigna un texto al elemento
+    h3.innerText = pokemonData.name
+    //Agregar un elemento como hijo
+    pokemonArticle.appendChild(h3)
+
+    //crear elemento imagen
+    const img = document.createElement('img')
+    img.src = pokemonData.sprites.front_default
+    pokemonArticle.appendChild(img)
+
+
+
+
+ })  
+    
+
+
 const obtenerPokemon = async () => {
     const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     const data = await respuesta.json()
     console.log(data)
+    return data
+
 }
 
-obtenerPokemon()
